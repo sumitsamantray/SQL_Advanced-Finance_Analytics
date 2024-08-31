5953f821131b612eba138e418b01ead61fac9b40
Sumit Samantray <162700659+sumitsamantray@users.noreply.github.com>
2024-08-31 22:50:49 +0530

    Add files via upload

--- Finance_Analytics/fact_forecast_monthly.sql
+++ Finance_Analytics/fact_forecast_monthly.sql
@@ -1,4 +1,7 @@
-select * from fact_sales_monthly
+select * 
+from fact_sales_monthly s
+join dim_product p
+on p.product_code = s.product_code
 where
 customer_code=90002002 and
 get_fiscal_year(date)=2021
