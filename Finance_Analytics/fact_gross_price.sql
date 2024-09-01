f91083fac2d5cc5e29c6a6d6f25f343b1124ae20
Sumit Samantray <162700659+sumitsamantray@users.noreply.github.com>
2024-09-01 15:22:03 +0530

    Add files via upload

---
+++ Finance_Analytics/fact_gross_price.sql
@@ -0,0 +1,19 @@
+
+
+
+
+
+select 
+s.date, s.product_code,
+p.product, p.variant, s.sold_quantity, g.gross_price
+from fact_sales_monthly s
+join dim_product p
+on p.product_code = s.product_code
+join fact_gross_price g 
+on 
+g.product_code = s.product_code and
+g.fiscal_year=get_fiscal_year(s.date)
+where customer_code = 90002002 and
+get_fiscal_year(date) = 2021
+order by date asc
+limit 100000
