9f16b24183f81ab6c35dbe4156d375f313d16168
Sumit Samantray <162700659+sumitsamantray@users.noreply.github.com>
2024-09-03 18:02:01 +0530

    Add files via upload

--- Finance_Analytics/total_gross_price.sql
+++ Finance_Analytics/total_gross_price.sql
@@ -1,10 +1,11 @@
-select 
+select
 s.date,
-g.gross_price * sold_quantity as gross_price_total
+sum(g.gross_price*s.sold_quantity) as gross_price_total
 from fact_sales_monthly s
 join fact_gross_price g
-on
-g.product_code = s.product_code and
-g.fiscal_year = get_fiscal_year(s.date)
-where customer_code = 90002002
+on 
+g.product_code=s.product_code and
+g.fiscal_year=get_fiscal_year(s.date)
+where customer_code=90002002
 group by s.date
+order by s.date asc
