8b1c06d6f62e77272fb6c01f6cf13f515451fef8
Sumit Samantray <162700659+sumitsamantray@users.noreply.github.com>
2024-09-04 22:32:53 +0530

    Add files via upload

---
+++ Finance_Analytics/pre_invoice_deduction_pct.sql
@@ -0,0 +1,22 @@
+SELECT 
+    	   s.date, 
+           s.product_code, 
+           p.product, 
+	   p.variant, 
+           s.sold_quantity, 
+           g.gross_price as gross_price_per_item,
+           ROUND(s.sold_quantity*g.gross_price,2) as gross_price_total,
+           pre.pre_invoice_discount_pct
+	FROM fact_sales_monthly s
+	JOIN dim_product p
+            ON s.product_code=p.product_code
+	JOIN fact_gross_price g
+    	    ON g.fiscal_year=get_fiscal_year(s.date)
+    	    AND g.product_code=s.product_code
+	JOIN fact_pre_invoice_deductions as pre
+            ON pre.customer_code = s.customer_code AND
+            pre.fiscal_year=get_fiscal_year(s.date)
+	WHERE 
+	    s.customer_code=90002002 AND 
+    	    get_fiscal_year(s.date)=2021     
+	LIMIT 1000000;
