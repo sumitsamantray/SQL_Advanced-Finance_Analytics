30e880fb0b885b25c8b64fe439d277779e272cd5
Sumit Samantray <162700659+sumitsamantray@users.noreply.github.com>
2024-09-06 14:09:18 +0530

    Update subqueries example 1.sql
    Adding comments

--- Subquries/subqueries example 1.sql
+++ Subquries/subqueries example 1.sql
@@ -1,5 +1,5 @@
-
 # select a movie with highest imdb_rating
 select * from moviesdb. movies
 where imdb_rating=(select max(imdb_rating) from moviesdb.movies)
-			    -- adding subqueries
+		   -- adding subqueries
+                   -- min max is just an aggregate function
