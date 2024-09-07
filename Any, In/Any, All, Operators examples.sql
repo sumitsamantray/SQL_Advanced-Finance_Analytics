230fd6e368ac53e5d1afe74dbbfd6b714061a85f
Sumit Samantray <162700659+sumitsamantray@users.noreply.github.com>
2024-09-07 17:02:35 +0530

    Update Any, All, Operators examples.sql

--- Any, In/Any, All, Operators examples.sql
+++ Any, In/Any, All, Operators examples.sql
@@ -1,6 +1,14 @@
 # select actors who acted in any of these movies 
 # (101, 110, 121)
 # use moviesdb
-select * from actors where actor_id in(
-SELECT actor_id from movie_actor where movie_id in 
-(101, 110, 121));
+SELECT 
+    *
+FROM
+    actors
+WHERE
+    actor_id IN (SELECT 
+            actor_id
+        FROM
+            movie_actor
+        WHERE
+            movie_id IN (101, 110, 121));
